856046063147ef10f9cb3e8538dc23707d79a779
birdkyle7918 <bbirdkkyle@gmail.com>
2025-09-07 21:54:45 +0800

    fix

--- src/tg_crawler/run.py
+++ src/tg_crawler/run.py
@@ -1,9 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "src.tg_crawler.main:app",
-        host="0.0.0.0",
-        port=8444,
-        reload=True
-    )
+    uvicorn.run("tg_crawler.main:app", host="0.0.0.0", port=8444, reload=True)
